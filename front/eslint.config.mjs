194c17fca3bcf537f42b00c10a333c663ef77455
morrison <morrison.k@hpcnt.com>
2024-09-08 15:46:21 +0900

    :wrench: Setting eslint/prettier for client

---
+++ front/eslint.config.mjs
@@ -0,0 +1,49 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
+import pluginImport from "eslint-plugin-import";
+import pluginPrettier from "eslint-plugin-prettier";
+import pluginReact from "eslint-plugin-react"; 
+
+export default [
+  {
+    files: ["**/*.{js,jsx}"], // Include .tsx files
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true, // Enable JSX parsing
+        },
+      },
+      globals: {
+        ...globals.browser,
+        es2020: true,
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      'react-refresh': pluginReactRefresh,
+      import: pluginImport,
+      prettier: pluginPrettier,
+    },
+    settings: { // Add this section
+      react: {
+        version: "detect", // Automatically detect the react version
+      },
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules, // Add this line
+      ...pluginReactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-hooks/exhaustive-deps': 'error',
+    },
+  },
+  {
+    ignores: ['dist', 'eslint.config.mjs'],
+  },
+];
