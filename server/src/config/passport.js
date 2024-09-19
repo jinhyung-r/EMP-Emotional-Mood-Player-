1b101b8740b480018ff743378d0b750b60ca5a4b
jh <jinhyung3012@gmail.com>
2024-09-20 04:26:32 +0900

    feat: passport strategy 수정

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -5,6 +5,7 @@ import passport from 'passport';
 import { findUserById } from '../services/userService.js';
 import googleStrategy from './strategies/googleStrategy.js';
 import spotifyStrategy from './strategies/spotifyStrategy.js';
+import logger from '../utils/logger.js';
 
 const configurePassport = () => {
   passport.use(googleStrategy);
@@ -14,17 +15,20 @@ const configurePassport = () => {
     done(null, user);
   });
 
+  //만료시간 필드 추가
   passport.deserializeUser(async (serializedUser, done) => {
     try {
-      const { id, provider, accessToken, refreshToken } = serializedUser;
+      const { id, provider, accessToken, refreshToken, expiresAt } = serializedUser;
       const user = await findUserById(id);
       if (user) {
         user.provider = provider;
         user.accessToken = accessToken;
         user.refreshToken = refreshToken;
+        user.expiresAt = expiresAt;
       }
       done(null, user);
     } catch (error) {
+      logger.error(`역직렬화 중 에러: ${error.message}`);
       done(error, null);
     }
   });
--- server/src/config/strategies/googleStrategy.js
+++ server/src/config/strategies/googleStrategy.js
@@ -1,6 +1,7 @@
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import config from '../index.js';
 import { findOrCreateUser } from '../../services/userService.js';
+import logger from '../../utils/logger.js';
 
 export default new GoogleStrategy({
   clientID: config.GOOGLE_CLIENT_ID,
@@ -9,13 +10,17 @@ export default new GoogleStrategy({
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const user = await findOrCreateUser(profile, 'google');
+    const expiresAt = Date.now() + 3600 * 1000; // 1시간 뒤(스포티파이가 1시간이니 일괄적으로 적용?)
+    logger.info(`구글 유저 로그인 성공: ${user.id}`);
     done(null, { 
       id: user.id,
       provider: 'google',
       accessToken,
-      refreshToken
+      refreshToken,
+      expiresAt
     });
   } catch (error) {
+    logger.error(`구글 로그인 에러: ${error.message}`);
     done(error, null);
   }
 });
