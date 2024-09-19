306018e1878cacee97a660c98a7554f0f79ae236
jh <jinhyung3012@gmail.com>
2024-09-20 04:30:20 +0900

    feat: 추가된 모듈들 app.js에 추가

--- server/src/app.js
+++ server/src/app.js
@@ -6,8 +6,10 @@ import * as config from './config/index.js';
 import sessionConfig from './config/session.js';
 import configurePassport from './config/passport.js';
 import routes from './routes/index.js';
-import db, { sequelize } from './models/index.js';
-
+import db from './models/index.js';
+import { checkAndRefreshToken } from './middlewares/tokenMiddleware.js';
+import logger from './utils/logger.js';
+import { errorHandler } from './middlewares/errorHanlder.js'
 const app = express();
 
 app.use(cookieParser(config.COOKIE_SECRET));
@@ -17,16 +19,20 @@ app.use(passport.session());
 
 configurePassport();
 
+app.use(checkAndRefreshToken);
+
 app.use('/', routes);
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('server error');
+app.use(errorHandler);
+
+app.use((err, req, res, _next) => {
+  logger.error(`Unhandled error: ${err.message}`, { stack: err.stack });
+  res.status(500).send('An unexpected error occurred');
 });
 
 db.sequelize.sync().then(() => {
     app.listen(config.PORT, () => {
-      console.log(`Server is running on port ${config.PORT}`);
+      logger.info(`Server is running on port ${config.PORT}`);
     });
 });
 
