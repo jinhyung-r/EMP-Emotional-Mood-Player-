15ecb3fbee623390ffb8b1a3c4ee83cb8d81d2d8
jh <jinhyung3012@gmail.com>
2024-09-19 13:38:23 +0900

    feat: logger(winston) 추가

---
+++ server/src/utils/logger.js
@@ -0,0 +1,30 @@
+// 에러와 전체 로그 구조화
+
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+
+// node 환경별로 production이 아닐경우 콘솔에 출력
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+
+export default logger;
