2c77239dcdc3ed73396f2fe8c439419f6a280d58
jh <jinhyung3012@gmail.com>
2024-09-20 21:37:21 +0900

    feat: 컨트롤러단 디버깅용 logger 추가

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,19 +1,35 @@
 // 사용자의 db에 따른 리다렉 경로 정의 필요
 // playlist 존재하면 playlist, 없으면 /create로..
 
-import { UserDTO } from '../middlewares/DTOs/index.js';
+import { UserDTO } from '../dtos/userDto.js';
+import logger from '../utils/logger.js';
+import config from '../config/index.js';
 
 export const oauthCallback = (req, res) => {
-  req.session.user = new UserDTO(req.user);
-  res.redirect('/create');
+  const user = req.user;
+  logger.info(`Raw user object: ${JSON.stringify(user)}`);
+
+  const userDto = UserDTO.fromEntity(user);
+  req.session.user = userDto.toJSON();
+
+  logger.info(`User authenticated: ${user.id}`);
+  logger.info(`Provider: ${user.provider}`);
+  logger.info(`Access Token: ${user.accessToken}`); 
+  logger.info(`Refresh Token: ${user.refreshToken}`);
+  logger.info(`Token expires at: ${new Date(user.expiresAt).toISOString()}`);
+  
+  // 이부분은 프론트엔드로 리다렉이니 프론트엔드와 협의 필요
+  // auth-success 라고 두었지만 뭐 정하고싶은대ㅐ로 지어도 상관없음, 그에 맞게 바꾸기
+  res.redirect(`${config.FRONTEND_URL}/auth-success`);
 };
 
 export const logout = (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      console.error('Error destroying session:', err);          // 역시 에러처리 필요
+      logger.error('세션 삭제 에러:', err);
     }
     res.clearCookie('auth_session');
-    res.redirect('/home');
+    logger.info(`유저 로그아웃 완료: ${req.user?.email}`);
+    res.redirect(`${config.FRONTEND_URL}/home`);
   });
 };
