a0a90daf51df439ec1dd775759903b924c6c7873
jh <jinhyung3012@gmail.com>
2024-09-20 23:25:05 +0900

    refactor: errorHandler

--- server/src/middlewares/errorHanlder.js
+++ server/src/middlewares/errorHanlder.js
@@ -1,8 +1,7 @@
-
 import logger from '../utils/logger.js';
 import { AppError } from '../utils/errors.js';
 
-export const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, req, res, _next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
@@ -12,9 +11,9 @@ export const errorHandler = (err, req, res, next) => {
     let error = { ...err };
     error.message = err.message;
 
-    if (error.name === 'CastError') error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    if (error.name === 'SequelizeValidationError') error = handleValidationErrorDB(error);
+    if (error.name === 'SequelizeUniqueConstraintError') error = handleDuplicateFieldsDB(error);
+    if (error.name === 'SequelizeDatabaseError') error = handleDatabaseErrorDB(error);
 
     sendErrorProd(error, res);
   }
@@ -31,39 +30,33 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message
     });
-  } 
-  // Programming or other unknown error: don't leak error details
-  else {
-    // 1) Log error
+  } else {
     logger.error(`Unexpected error: ${err.message}`, { stack: err.stack });
-
-    // 2) Send generic message
     res.status(500).json({
       status: 'error',
-      message: 'An unexpected error occurred. Please try again later.'
+      message: 'Something went wrong'
     });
   }
 };
 
-const handleCastErrorDB = err => {
-  const message = `Invalid ${err.path}: ${err.value}.`;
+const handleValidationErrorDB = err => {
+  const errors = err.errors.map(e => e.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
 
 const handleDuplicateFieldsDB = err => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  const message = `Duplicate field value: ${value}. Please use another value.`;
+  const field = err.errors[0].path;
+  const message = `Duplicate field value: ${field}. Please use another value!`;
   return new AppError(message, 400);
 };
 
-const handleValidationErrorDB = err => {
-  const errors = Object.values(err.errors).map(el => el.message);
-  const message = `Invalid input data. ${errors.join('. ')}`;
-  return new AppError(message, 400);
+const handleDatabaseErrorDB = err => {
+  const message = `Database error: ${err.parent.sqlMessage}`;
+  return new AppError(message, 500);
 };
