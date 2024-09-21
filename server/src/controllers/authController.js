3600a87165a94db273c9d7b245785c5e2dfc4fbb
jh <jinhyung3012@gmail.com>
2024-09-21 17:50:53 +0900

    feat: json응답 넘기는 방식 추가(차후 수정필요)

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,6 +1,3 @@
-// 사용자의 db에 따른 리다렉 경로 정의 필요
-// playlist 존재하면 playlist, 없으면 /create로..
-
 import { UserDTO } from '../dtos/index.js';
 import logger from '../utils/logger.js';
 import config from '../config/index.js';
@@ -12,15 +9,23 @@ export const oauthCallback = (req, res) => {
   const userDto = UserDTO.fromEntity(user);
   req.session.user = userDto.toJSON();
 
+  // 로거는 나중에 지우기
   logger.info(`User authenticated: ${user.id}`);
   logger.info(`Provider: ${user.provider}`);
   logger.info(`Access Token: ${user.accessToken}`); 
   logger.info(`Refresh Token: ${user.refreshToken}`);
   logger.info(`Token expires at: ${new Date(user.expiresAt).toISOString()}`);
   
-  // 이부분은 프론트엔드로 리다렉이니 프론트엔드와 협의 필요
-  // auth-success 라고 두었지만 뭐 정하고싶은대ㅐ로 지어도 상관없음, 그에 맞게 바꾸기
-  res.redirect(`${config.FRONTEND_URL}/auth-success`);
+  // 일단은 벡에서 프론트로 json응답을 보내는 방식으로 사용(200코드 보내는 방식도 고려해보면 좋을듯)
+  // 유저에 따른 응답값 구현 필요(아직 모델 구성 전이라 그 후에 진행)
+  // 아예 json에 플레이리스트 값을 추가해서 보내는 방식도 괜찮을듯? -> 인가 완료 및 플레이리스트 없음 or 있음
+  res.json({
+    success: true,
+    user: userDto.toJSON(),
+    message: '인가 완료'
+  });
+
+  // res.status(200)?
 };
 
 export const logout = (req, res) => {
