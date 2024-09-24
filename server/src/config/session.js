fd49cc034106db434225907ef1eb88a99be9b758
jh <jinhyung3012@gmail.com>
2024-09-24 18:05:45 +0900

    feat: signed cookie 활성화

--- server/src/config/session.js
+++ server/src/config/session.js
@@ -12,6 +12,7 @@ const sessionStore = new MySQLStoreSession({
     db: config.MYSQL_NAME,
 });
 
+// httpOnly + signed cookie
 const sessionConfig = {
     key: 'auth_session',
     secret: config.SESSION_SECRET,
@@ -22,7 +23,8 @@ const sessionConfig = {
         httpOnly: true,
         secure: false,              // http환경에서만 작동 -> 배포할시에는 https로 배포할건이니 true로 설정 혹은 환경변수로 두고 변경하는게 더 편리해보임
         sameSite: 'strict',
-        maxAge: 24 * 60 * 60 * 1000 * 3     // 3일
+        maxAge: 24 * 60 * 60 * 1000 * 3,    // 3일
+        signed: true
     }
 };
 
