915bc94ea9bc9e7e64201b2175c3161f9cd73347
jh <jinhyung3012@gmail.com>
2024-09-24 18:23:17 +0900

    refactor: 토큰 만료 갱신 로직 최적화 및 에러처리

--- server/src/middlewares/tokenMiddlware.js
+++ server/src/middlewares/tokenMiddlware.js
@@ -1,5 +1,6 @@
 import { checkAndRefreshTokenIfNeeded } from '../services/authService.js';
 import logger from '../utils/logger.js';
+import { UnauthorizedError, InternalServerError } from '../utils/errors.js';
 
 export const checkAndRefreshToken = async (req, res, next) => {
   if (req.isAuthenticated() && req.user) {
@@ -7,10 +8,25 @@ export const checkAndRefreshToken = async (req, res, next) => {
       const tokenRefreshed = await checkAndRefreshTokenIfNeeded(req.user);
       if (tokenRefreshed) {
         req.session.user = req.user;
+        await new Promise((resolve, reject) => {
+          req.session.save((err) => {
+            if (err) reject(new InternalServerError('세션 저장이 실패했습니다.', err.message));
+            else resolve();
+          });
+        });
       }
     } catch (error) {
-      logger.error(`토큰 리프레시 에러: ${error.message}`);
-      return res.status(401).json({ message: "인증 중 로그인에러, 재로그인해주세요" });
+      if (error instanceof UnauthorizedError) {
+        req.logout((err) => {
+          if (err) {
+            logger.error(`로그아웃 에러!: ${err.message}`);
+          }
+          return res.status(error.statusCode).json({ message: error.message, details: error.details });
+        });
+      } else {
+        logger.error(`토큰 refresh 에러!: ${error.message}`);
+        return res.status(error.statusCode || 500).json({ message: error.message, details: error.details });
+      }
     }
   }
   next();
--- server/src/utils/tokenUtils.js
+++ server/src/utils/tokenUtils.js
@@ -1,3 +1,20 @@
+import { BadRequestError } from "./errors";
+
+// token expiresAt 사용해서 만료 10분 전부터 갱신 시도
+// 상수니까 대문자 + 언더스코어로 명명(JS  컨벤션?)
+const REFRESH_THRESHOLD = 10 * 60 * 1000; // 10분
+
+export const isTokenExpiringSoon = (expiresAt) => {
+  if (!expiresAt) {
+    throw new BadRequestError('만료시간(expiresAt)이 유효하지 않습니다.');
+  }
+  const now = Date.now();
+  return expiresAt - now < REFRESH_THRESHOLD;
+};
+
 export const isTokenExpired = (expiresAt) => {
+  if (!expiresAt) {
+    throw new BadRequestError('만료시간(expiresAt)이 유효하지 않습니다.');
+  }
   return Date.now() >= expiresAt;
 };
