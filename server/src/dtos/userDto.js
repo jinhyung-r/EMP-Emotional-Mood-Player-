6b562e385c706b5634a36d56ab6283be4457139e
jh <jinhyung3012@gmail.com>
2024-09-25 20:22:18 +0900

    hotfix: 급하게 userdto validation제거

--- server/src/dtos/userDto.js
+++ server/src/dtos/userDto.js
@@ -1,5 +1,3 @@
-import { BadRequestError } from '../utils/errors.js';
-
 export class UserDTO {
   #id;
   #provider;
@@ -7,37 +5,9 @@ export class UserDTO {
   #expiresAt;
 
   constructor(id, provider, email, expiresAt) {
-    this.setId(id);
-    this.setProvider(provider);
-    this.setEmail(email);
-    this.setExpiresAt(expiresAt);
-  }
-
-  setId(id) {
-    if (!id || typeof id !== 'number') {
-      throw new BadRequestError('유효하지 않은 ID');
-    }
     this.#id = id;
-  }
-
-  setProvider(provider) {
-    if (!provider || typeof provider !== 'string') {
-      throw new BadRequestError('유효하지 않은 provider');
-    }
     this.#provider = provider;
-  }
-
-  setEmail(email) {
-    if (!email || typeof email !== 'string' || !email.includes('@')) {
-      throw new BadRequestError('유효하지 않은 email');
-    }
     this.#email = email;
-  }
-
-  setExpiresAt(expiresAt) {
-    if (!expiresAt || typeof expiresAt !== 'number' || expiresAt <= Date.now()) {
-      throw new BadRequestError('유효하지 않은 만료시간');
-    }
     this.#expiresAt = expiresAt;
   }
 
