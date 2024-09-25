286de8ee1d0fee25028443258e63a245e1840a46
jh <jinhyung3012@gmail.com>
2024-09-25 19:24:16 +0900

    refactor: userDto

--- server/src/dtos/userDto.js
+++ server/src/dtos/userDto.js
@@ -1,23 +1,16 @@
 import { BadRequestError } from '../utils/errors.js';
 
-
-// 물어보기
 export class UserDTO {
   #id;
   #provider;
   #email;
   #expiresAt;
-  // 엑세스토큰이랑 리프레시토큰을 dto에 넣는게 맞는가??
-  #accessToken;
-  #refreshToken;
 
-  constructor(id, provider, email, expiresAt, accessToken, refreshToken) {
+  constructor(id, provider, email, expiresAt) {
     this.setId(id);
     this.setProvider(provider);
     this.setEmail(email);
     this.setExpiresAt(expiresAt);
-    this.setAccessToken(accessToken);
-    this.setRefreshToken(refreshToken);
   }
 
   setId(id) {
@@ -48,47 +41,26 @@ export class UserDTO {
     this.#expiresAt = expiresAt;
   }
 
-  // 미정
-  setAccessToken(accessToken) {
-    if (!accessToken || typeof accessToken !== 'string') {
-      throw new BadRequestError('유효하지 않은 accesstoken');
-    }
-    this.#accessToken = accessToken;
-  }
-
-  // 미정
-  setRefreshToken(refreshToken) {
-    if (!refreshToken || typeof refreshToken !== 'string') {
-      throw new BadRequestError('유효하지 않은 refreshtoken');
-    }
-    this.#refreshToken = refreshToken;
-  }
-
   getId() { return this.#id; }
   getProvider() { return this.#provider; }
   getEmail() { return this.#email; }
   getExpiresAt() { return this.#expiresAt; }
-  // 미정
-  getAccessToken() { return this.#accessToken; }
-  getRefreshToken() { return this.#refreshToken; }
 
   static fromEntity(user) {
     return new UserDTO(
       user.id,
       user.provider,
-      user.accessToken,
-      user.refreshToken,
+      user.email,
       user.expiresAt
     );
   }
 
   toJSON() {
     return {
-      id: this.id,
-      provider: this.provider,
-      accessToken: 'Stored securely',
-      refreshToken: this.refreshToken ? 'Stored securely' : null,
-      expiresAt: this.expiresAt
+      id: this.#id,
+      provider: this.#provider,
+      email: this.#email,
+      expiresAt: this.#expiresAt
     };
   }
 }
