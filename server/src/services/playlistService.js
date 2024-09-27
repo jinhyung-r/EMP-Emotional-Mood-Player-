e710580de96c4167e80ba71b4a3f7c804d1be4eb
ny <xokny04188@naver.com>
2024-09-27 16:45:33 +0900

    mypage

--- server/src/services/playlistService.js
+++ server/src/services/playlistService.js
@@ -2,10 +2,10 @@ import prisma from '../models/index.js';
 import logger from '../utils/logger.js';
 import { NotFoundError } from '../utils/errors.js';
 
-export const getUserPlaylists = async (userId) => {
+export const getUserPlaylists = async (playlistId) => {
     try {
         const playlists = await prisma.playlist.findMany({
-            where: { userId: userId},
+            where: { playlistId: parseInt(playlistId, 10) }, 
             include: {
                 tracks: true,
             },
@@ -25,14 +25,33 @@ export const getUserFirstPlaylist = async (userId) => {
   try {
     const playlist = await prisma.playlist.findFirst({
       where: { userId: userId },
-      include: {
-        tracks: true,
+      select: {
+        playlistId: true, // playlistId만 선택
       },
     });
 
-    return playlist || null; // 플레이리스트가 없으면 null 반환
+    return playlist ? playlist.playlistId : null; // playlistId가 없으면 null 반환
   } catch (error) {
     logger.error(`첫 번째 플레이리스트 조회 중 오류 발생: ${error.message}`);
     return null; // 에러 발생 시에도 null 반환
   }
+};
+
+export const getPlaylistById = async (playlistId) => {
+    try {
+        const playlist = await prisma.playlist.findUnique({
+            where: { playlistId: parseInt(playlistId, 10) }, 
+            include: {
+                tracks: true,
+            },
+        });
+        if (!playlist) {
+            throw new NotFoundError('플레이리스트를 찾을 수 없습니다.');
+        }
+
+        return playlist;
+    } catch (error) {
+        logger.error(`플레이리스트 조회 중 오류: ${error.message}`);
+        throw error;
+    }
 };
