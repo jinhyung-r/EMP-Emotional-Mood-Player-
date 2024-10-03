a04d87198870a6943709c0b917ecc188ad878ea9
jh <jinhyung3012@gmail.com>
2024-10-04 02:56:43 +0900

    hotfix: post save playlist

--- server/src/routes/playlistRoutes.js
+++ server/src/routes/playlistRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createLyricsPlaylist, createEmotionPlaylist, getPlaylistByIdHandler, updatePlaylistTitleHandler, deletePlaylistHandler } from '../controllers/playlistController.js';
+import { createLyricsPlaylist, createEmotionPlaylist, getPlaylistByIdHandler, updatePlaylistTitleHandler, deletePlaylistHandler, savePlaylist } from '../controllers/playlistController.js';
 import { isAuthenticated } from '../middlewares/auth.js';
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post('/lyrics-playlist', isAuthenticated, createLyricsPlaylist);
 router.post('/emotion-playlist', isAuthenticated, createEmotionPlaylist);
+router.post('/save-playlist', isAuthenticated, savePlaylist);
 router.get('/myplaylist/:playlistId', isAuthenticated, getPlaylistByIdHandler);
 router.put('/myplaylist/:playlistId', isAuthenticated, updatePlaylistTitleHandler);
 router.delete('/myplaylist/:playlistId',isAuthenticated, deletePlaylistHandler);
